b9e9be8d1f0bbdd1b0400ab47301f1ad8f8e71ae
atlem <atle.mjolhus@live.no>
2025-02-13 12:24:55 +0100

    Add files via upload

---
+++ .github/Product.js
@@ -0,0 +1,29 @@
+export const jsonData =
+{
+    "Coffee": [
+      { "name": "Nano Matcha", "price": 5.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/nanomatcha.png" },
+      { "name": "Codespresso", "price": 4.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/codespresso.png" },
+      { "name": "Lattech", "price": 4.50, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/lattech.png" },
+      { "name": "Cocoa Peak", "price": 6.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/cocoapeak.png" },
+      { "name": "Cyberbrew Special", "price": 6.50, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/nanomatcha.png" },
+      { "name": "Espresso Royale", "price": 4.75, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/nanomatcha.png" },
+      { "name": "Vanilla Cloud", "price": 5.20, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/nanomatcha.png" },
+      { "name": "Mocha Bliss", "price": 5.80, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/nanomatcha.png" }
+    ],
+    "Pastries": [
+      { "name": "Pastries Matcha", "price": 3.50, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/cheesecake.png" },
+      { "name": "CodeMatcha", "price": 3.80, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/extraCream.png" },
+      { "name": "Matchatech", "price": 3.20, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/muffin.png" },
+      { "name": "Cocoa Matcha", "price": 3.90, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/chocolateSyrup.png" },
+      { "name": "Almond Croissant", "price": 4.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/chocolateSyrup.png" },
+      { "name": "Chocolate Danish", "price": 4.50, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/chocolateSyrup.png" },
+      { "name": "Berry Tart", "price": 5.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/chocolateSyrup.png" },
+      { "name": "Honey Muffin", "price": 3.75, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/chocolateSyrup.png" }
+    ],
+    "Smoothies": [
+      { "name": "Tropical Fusion", "price": 6.00, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/blueberrySmoothie.png" },
+      { "name": "Berry Blast", "price": 5.50, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/strawberrySmoothie.png" },
+      { "name": "Green Detox", "price": 6.20, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/blueberrySmoothie.png" },
+      { "name": "Peach Sunrise", "price": 5.80, "image": "https://encryptia-learning-files.s3.eu-north-1.amazonaws.com/images/matcha.png" }
+    ]
+  };
